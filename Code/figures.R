f99f90aa4b2d311f8e7b0941947dcf276f819473
Hannah <direch@uw.edu>
2016-04-14 22:38:11 -0700

    uploading current

---
+++ Code/figures.R
@@ -0,0 +1,94 @@
+###Hannah Director###
+#Research paper prelim 
+rm(list = ls())
+source("/users/hdirector/Dropbox/Prelim/ReplicationCode/functions.R")
+
+#Constants
+DELTA <- 2
+
+#Convert data from matlab to R
+library("R.matlab")
+drifterbetty2 <- readMat("/users/hdirector/Dropbox/Prelim/ReplicationCode/drifterbetty2.mat")
+drift <- list("num" = as.vector(drifterbetty2$drifterbetty[,,1]$num),
+              "lat" = as.vector(drifterbetty2$drifterbetty[,,1]$lat),
+              "lon" = as.vector(drifterbetty2$drifterbetty[,,1]$lon),
+              "cv" = as.vector(drifterbetty2$drifterbetty[,,1]$cv),
+              "f" = as.vector(drifterbetty2$drifterbetty[,,1]$f))
+
+
+###Figure 1 (right)###
+#Indices of sections of interest
+startTs <- 1901; endTs <- 4900 #index of period of interest is obtained from Sykulski et al posted Matlab code
+bStartTs <- 2971; bEndTs <-3570 #index of 50 day period in blue is obtained from Sykulski et al posted Matlab code
+gStartTs <- 4201; gEndTs <- 4800 #index of 50 day period in blue is obtained from Sykulski et al posted Matlab code
+
+#plot time series, coloring particular sections 
+plot(drift$lon[startTs:endTs], drift$lat[startTs:endTs], type = "l", 
+     xlab = "Longitude", ylab = "Latitude", main = "Replication of Figure 1 (Right)")
+points(drift$lon[gStartTs:gEndTs], drift$lat[gStartTs:gEndTs], type = "l", col = "green")
+points(drift$lon[bStartTs:bEndTs], drift$lat[bStartTs:bEndTs], type = "l", col = "blue")
+
+###Figure 2###
+#To do: figure out how to get complex valued velocities (e.g. units)
+#Calculate velocities u(t) (horizontal) and v(t) (vertical)
+
+#pull out complex valued velocities
+bZ <- drift$cv[bStartTs:bEndTs]
+gZ <- drift$cv[gStartTs:gEndTs]
+
+#get periodogram
+blue <- getPerio(Z = bZ, delta = DELTA)
+green <- getPerio(Z = gZ, delta = DELTA)
+
+#Find inertial frequency (PROBLEM HERE)
+##TO DO: I AM CALCULATING THE INERTIAL FREQUENCY WRONG IN SOME WAY
+#T <- (23*60*60) + (56*60) + 4.1 #time to rotate around Earth once
+#psi <- 2*pi/T #rate of Earth's rotation
+#bLat <- (pi/2)*(drift$lat[bStartTs:bEndTs]/90)
+#bf0 <- 2*pi*-2*psi*sin(bLat)*1000
+#mean(bf0)
+
+#plot figure
+par(mfrow = c(1, 2))
+plot(DELTA*blue$omega, blue$sZ, type = "l", col = "blue", xlab = expression(paste(omega, Delta)),
+     ylab = "dB", xlim = c(-pi, pi), ylim = c(-20, 60))
+#abline(v = f0, col = "red")
+plot(DELTA*green$omega, green$sZ, type = "l", col = "green", xlab = expression(paste(omega, Delta)),
+     ylab = "dB", xlim = c(-pi, pi), ylim = c(-20, 60))
+mtext("Replication of Figure 2", outer = T, line = -3)
+#abline(v = f0, col = "red")
+
+###Figure 3
+rm(list = ls())
+source("/users/hdirector/Dropbox/Prelim/ReplicationCode/functions.R")
+set.seed(103)
+
+###TO DO: How did they come up with these variance (autocorrelation at 0)?
+
+#sample matern
+N <- 1000
+DELTA <- 2
+matSamp <- simMatern(B = 10, alpha  = 0.9, h = 0.1, N = N)
+mat <- getPerio(Z = matSamp, delta = DELTA)
+
+#sample FBM
+N <- 1000
+fbmSamp <- simFBM(B = 10, alpha  = 0.9, H = 0.1, N = N)
+fbm <- getPerio(fbmSamp, delta = DELTA)
+
+#plot figure
+layout(matrix(c(1, 2, 3, 3), 2, 2, byrow = TRUE))
+plot(fbmSamp[,"u"], col = "blue", xlim = c(0, 1000), ylim = c(-200, 200),
+     type = "l", xlab = expression(paste(t, Delta)), ylab = "u (cm/s)")
+points(matSamp[,"u"], col = "red", type = "l")
+mtext("Realization of Figure 3 \n (Note: Figure is not identical, since it represents a realization of a random processes)", 
+      outer = T, line = -3)
+plot(fbmSamp, col = "blue", xlim = c(-200, 200), ylim = c(-200, 200),
+     xlab = "u (cm/s)", ylab = "v (cm/s)", type = "l")
+points(matSamp, col = "red", type = "l")
+plot(DELTA*fbm$omega, fbm$sZ, col = "blue", ylim = c(-20, 80), xlim = c(-pi, pi),
+     type = "l", xlab = expression(paste(omega, Delta)), ylab = "dB")
+points(DELTA*mat$omega, mat$sZ, col = "red", type = "l")
+
+
+
---
+++ Code/functions.R
@@ -0,0 +1,88 @@
+#Calculate periodogram
+getPerio <- function(Z, delta, dB = TRUE) {
+  library("waved")
+  #fourier frquencies 
+  N <- length(Z)
+  if (N%%2 == 1) {
+    omega <- (2*pi/(N*delta))*c(sort(-seq(1, (N - 1)/2, 1)), seq(1, (N - 1)/2, 1))
+  } else {
+    omega <- (2*pi/(N*delta))*c(sort(-seq(1, N/2, 1)), seq(1, N/2, 1))
+  }
+  #convert to decibals
+  sZ <- (delta/N)*Mod(fft(Z))^2; sZ <- fftshift(sZ)
+  if (dB == TRUE) {
+    sZ <- 10*log10(sZ)
+  }
+  return(list("sZ" = sZ, "omega" = omega))
+}
+
+#Function to get autocorrelation for the matern
+maternAc <- function(B, alpha, h, N) {
+  ac <- rep(NA, N)
+  ac[1] <- ((B^2)*beta(0.5, alpha - 0.5))/(2*pi*abs(h)^(2*alpha  - 1)) #variance (cov at lag 0) 
+  tau <- 1:(N - 1) #lags 
+  ac[2:N] <- ((B^2*(h*abs(tau))^(alpha - 0.5)*besselK(h*abs(tau), alpha - 0.5))/
+                (2^(alpha - 1/2)*pi^(1/2)*gamma(alpha)*h^(2*alpha  - 1)))
+  return(ac)
+}
+
+#Function to get autocorrelation for the OU process 
+#(TO DO: DETERMINE IF TYPO IS IN THERE CODE OR PAPER 
+#(should there be a delta in this function)
+ouAc <- function(A, w0, c, tau, N) {
+  ac <- rep(NA, N)
+  tau <- 0:(N - 1) #lags 
+  ac <- (0i + A^2/abs(2*c))*exp(1i*w0*tau)*exp(0i + -c*abs(tau))
+  return(ac)
+}
+
+#Function to simulate a time series with complex Matern covariance
+simMatern <- function(B, alpha, h, N) {
+  
+  #calculate autocorrelation
+  ac <- maternAc(B, alpha, h, N)
+  
+  #Create covariance and generate realizations using standard gaussian tricks
+  library(mvtnorm)
+  L <- t(chol(toeplitz(ac)))
+  u <- rmvnorm(N, mean = rep(0, 2), sigma = diag(2))
+  Z <- L%*%u/sqrt(2) #renormalize variance
+  colnames(Z) <- c("u", "v")
+  return(Z)
+}
+
+#function to simulate a time series with fractional brownian motion
+simFBM <- function(B, alpha, H, N) {
+  #Create covariance and generate realizations using standard gaussian tricks
+  L <- matrix(nrow = N, ncol = N, data = NA)
+  sigma2 <- (B^2)*(-gamma(2-2*(alpha - 0.5))*cos(pi*(alpha - 0.5))/(pi*(alpha - 0.5)*(2*alpha - 2)))
+  H <- 2*alpha - 1
+  for(s in 1:N) {
+    for(t in 1:N) {
+      #fraction brownian motion covariance
+      L[t, s] <- (1/2)*(sigma2)*(t^H - abs(t -s)^H + s^H) 
+    }
+  }
+  L <- t(chol(L))
+  u <- rmvnorm(N, mean = rep(0, 2), sigma = diag(2))
+  Z <- L%*%u/sqrt(2) #renormalize variance
+  colnames(Z) <- c("u", "v")
+  return(Z)
+}
+
+
+#Blurred Whittle likelihood
+ll <- function(theta, Z, delta) {
+  N <- length(Z)
+  tau <-  seq(0, N - 1, 1)
+  curr <- getPerio(Z, delta)
+  sTau <- ouAc(A = 5, w0 = 2, c = 3, tau, N) + maternAc(B = 10, alpha = 2, h = 2, N)  
+  sBar <- Re(fftshift(2*fft(sTau*(1 - tau/N)) - sTau[1])) 
+  return(-sum(curr$sZ/sBar + log(sBar)))
+}
+
+
+#TESTING 
+theta <- c(10, .9, 1, 2, 2, 2)
+Z <- readMat("/users/hdirector/Dropbox/Prelim/ReplicationCode/samp.mat")$X
+  
