620ec65574b6fe55612eb10e25ccbf92c247accb
Hannah <direch@uw.edu>
2016-04-17 12:09:09 -0700

    qualitatively working, slight diff in optimized par values:

--- Code/dataConversion.m
+++ Code/dataConversion.m
@@ -1,5 +1,9 @@
 %Hannah Director
 %Prelim Exam, file conversion steps
-addpath('/users/hdirector/Dropbox/Prelim/MatlabCode/Figs124')
-load drifterbetty2.mat
-save('drifterbetty2.mat', '-v6')  
+%addpath('/users/hdirector/Documents/prelim/prelim/MatlabCode/Figs124')
+%load drifterbetty2.mat
+%save('/users/hdirector/Documents/prelim/prelim/Code/drifterbetty2.mat', '-v6') 
+
+addpath('/users/hdirector/Documents/prelim/prelim/MatlabCode/Fig5')
+load newinertial.mat
+save('/users/hdirector/Documents/prelim/prelim/Code/newinertial.mat', '-v6')
